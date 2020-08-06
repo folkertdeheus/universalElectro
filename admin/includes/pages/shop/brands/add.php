21f9702cef24e5d634d151e20211cc691381418c
folkertdeheus <60042971+folkertdeheus@users.noreply.github.com>
2020-08-06 16:48:10 +0200

    Added "add brand" form field check
    added live form filling check

--- admin/includes/pages/shop/brands/add.php
+++ admin/includes/pages/shop/brands/add.php
@@ -11,18 +11,19 @@ if (login()) {
 
     <div class="content">
         <form method="post" enctype="multipart/form-data" action="index.php?page=2&action=3">
-            <input type="text" name="name" placeholder="Name" required />
+            <input type="text" id="name" name="name" placeholder="Name" required onchange="addbrand('name')" />
             <label for="image">Image (max 2Mb):</label>
             <input type="file" id="image" name="image" placeholder="Image" />
-            <input type="text" name="website" placeholder="Website" />
-            <textarea name="description" placeholder="Description"></textarea>
+            <input type="text" id="website" name="website" placeholder="Website" />
+            <textarea id="website" name="description" placeholder="Description"></textarea>
             <input type="submit" value="Submit" />
             <input type="hidden" name="form" value="addBrand" />
         </form>
-    </div> <!-- content -->
 
-    <div class="message" id="message">
-    </div> <!-- message -->
+        <div class="message" id="message">
+        </div> <!-- message -->
+
+    </div> <!-- content -->
 
 <?php
 
