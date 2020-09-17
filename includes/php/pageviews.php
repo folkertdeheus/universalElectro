f044465aefc04372a6e7d5e82d35a2e9fac46d13
folkertdeheus <60042971+folkertdeheus@users.noreply.github.com>
2020-09-18 00:44:41 +0200

    Editted page to set null values
    Set null values if the server does not return requested values

--- includes/php/pageviews.php
+++ includes/php/pageviews.php
@@ -4,5 +4,11 @@
  * This file contains logging of the pageview
  */
 
+$ip = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '0.0.0.0';
+$agent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : null;
+$referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : null;
+$protocol = isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : null;
+$method = isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : null;
+
 // Insert pageview
-$q->addPageview($_SERVER['REMOTE_ADDR'], $_SERVER['HTTP_USER_AGENT'], $_SERVER['HTTP_REFERER'], $_SERVER['SERVER_PROTOCOL'], $_SERVER['REQUEST_METHOD']);
+$q->addPageview($ip, $agent, $referer, $protocol, $method);
